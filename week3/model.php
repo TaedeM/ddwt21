4d10438d5a0c8dc185574948c0a4d7cc8f51d561
TaedeM <taede.meijer@gmail.com>
2021-12-09 15:57:29 +0100

    New functions for the model

--- week3/model.php
+++ week3/model.php
@@ -258,3 +258,31 @@ function redirect($location){
     header(sprintf('Location: %s', $location));
     die();
 }
+
+function set_cred($username, $password){
+    return [
+        'username'=> $username,
+        'password'=> $password
+    ];
+}
+
+function check_cred($cred){
+    if (!isset($_SERVER['PHP_AUTH_USER'])){
+        return False;
+    }
+    else {
+        if ($_SERVER['PHP_AUTH_USER'] != $cred['username']){
+            return False;
+        }
+        elseif ($_SERVER['PHP_AUTH_PW'] != $cred['password']){
+            return False;
+        }
+        else {
+            return True;
+        }
+    }
+}
+
+function http_content_type($type){
+    return header('Content-type: '.$type);
+}
